e7c29af22dff401e9c961229ea1507f5f4a5c798
Clovis22 <clovis_jack@hotmail.com>
2026-05-15 15:25:33 -0300

    Adição de testes básicos do sistema SpringBoot
    Commit: Classe de testes e validação do sistema principal, a seguir:
    
    SgsApplicationTests.java

---
+++ sgs/sgs/src/test/java/com/sergipetec/sgs/SgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergipetec.sgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
